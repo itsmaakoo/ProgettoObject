7c1e1d9fafbe7eed3f458c8ab51654f1ee487202
Michele Lo Franco <michelelofranco2@gmail.com>
2025-07-17 15:27:23 +0200

    rimossa vecchia versione del progetto

--- src/model/StatoDelVolo.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum StatoDelVolo {
-    IN_RITARDO, ATTERRATO, CANCELLATO, DECOLLATO
-
-}
--- src/model/stato_bagaglio.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum stato_bagaglio {
-    imbarcato, ritirabile, smarrito, registrato;
-}
